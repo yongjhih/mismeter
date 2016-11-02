a3f485b5cae0b70893061397d83cfb7d138d0014
Andrew Chen <yongjhih@gmail.com>
2016-11-02 21:58:45 +0800

    Typo

--- app/src/main/kotlin/com/github/yongjhih/mismeter/app/FullscreenActivity.kt
+++ app/src/main/kotlin/com/github/yongjhih/mismeter/app/FullscreenActivity.kt
@@ -71,7 +71,12 @@ class FullscreenActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
 
-        mContentView!!.systemUiVisibility = View.SYSTEM_UI_FLAG_LOW_PROFILE or View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        mContentView.systemUiVisibility = View.SYSTEM_UI_FLAG_LOW_PROFILE or
+                View.SYSTEM_UI_FLAG_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
     }
 
     override fun onDestroy() {
